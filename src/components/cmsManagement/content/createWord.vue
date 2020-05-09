5eb81a1501ff69fe29a3411c4b7535ddb7cfd50d
leesunner <875784820@qq.com>
2020-05-09 11:37:35 +0800

    发布文章新增类型简称

--- src/components/cmsManagement/content/createWord.vue
+++ src/components/cmsManagement/content/createWord.vue
@@ -72,7 +72,10 @@
                     <!--&gt;{{item.value}}</el-radio>-->
                   <!--</el-radio-group>-->
                 <!--</el-form-item>-->
-                <el-form-item label="是否显示">
+                <el-form-item label="类型简称">
+                  <el-input clearable placeholder="请输入文章类型简称" v-model="columnData.shortName"></el-input>
+                </el-form-item>
+                <el-form-item label="是否显示" style="margin-left: 50px;">
                   <el-tooltip :content="columnData.hideStatus?'隐藏':'显示'" placement="right">
                     <el-switch
                       v-model="columnData.hideStatus"
@@ -201,6 +204,7 @@ import ElRow from "element-ui/packages/row/src/row";
         description: "",
         categoryId: "",
         author: "",
+          shortName:"",
         // type: "", // 1、国内新闻 2、县级新闻 3、通知公告
         copyFrom: "",
         keywords: "",
@@ -219,6 +223,7 @@ import ElRow from "element-ui/packages/row/src/row";
         // type: [
         //   { required: true, message: "请选择文章分类", trigger: "change" }
         // ],
+        shortName: [{ required: true, message: "请输入类型简称", trigger: "blur" }],
         image: [
           { required: true, message: "文章封面图不能为空", trigger: "blur" }
         ]
@@ -238,7 +243,8 @@ import ElRow from "element-ui/packages/row/src/row";
           description: "",
           categoryId: "",
           author: "",
-          type: "", // 1、国内新闻 2、县级新闻 3、通知公告
+          // type: "", // 1、国内新闻 2、县级新闻 3、通知公告
+          shortName:'',
           copyFrom: "",
           keywords: "",
           hideStatus: false //默认显示发布
